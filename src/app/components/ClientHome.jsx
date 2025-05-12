206cc9689ca2bb62e32b8fba5a5039404aaf8a8f
B231960019 <monhochir0131@gmail.com>
2025-05-12 23:43:44 +0800

    commit3

--- src/app/components/ClientHome.jsx
+++ src/app/components/ClientHome.jsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import dynamic from "next/dynamic";
 import BirthdayScreen from "./BirthdayScreen";
 
-// This dynamic import is now inside a client component
 const StartScreen = dynamic(() => import("./StartScreen"), { ssr: false });
 
 export default function ClientHome() {
